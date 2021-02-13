c72f7d54ecef9c52bdd0dbdb4fb93834985b0e3a
borislavatblagoev <borislavatblagoev@gmail.com>
2021-02-13 16:53:11 +0200

    Implement response.

---
+++ BoroServer.HTTP/ResponseCookie.cs
@@ -0,0 +1,31 @@
+﻿namespace BoroServer.HTTP
+{
+    using System.Text;
+
+    public class ResponseCookie : Cookie
+    {
+        public ResponseCookie(string cookieLine) 
+            : base(cookieLine)
+        {
+            this.Path = "/";
+        }
+
+        public bool IsHttpOnly { get; set; }
+
+        public int MaxAge { get; set; }
+
+        public string Path { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{this.Name}={this.Value}; Path={this.Path}; {(this.IsHttpOnly ? "HttpOnly;" : string.Empty)}");
+            if (this.MaxAge != 0)
+            {
+                sb.Append($" Max-Age={this.MaxAge};");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ BoroServer.HTTP/StatusCode.cs
@@ -0,0 +1,14 @@
+﻿namespace BoroServer.HTTP
+{
+    public enum StatusCode
+    {
+        Ok = 200,
+        Created = 201,
+        MovedPermanentlyRedirected = 301,
+        Redirect = 302,
+        BadRequest = 400,
+        Unauthorized = 401,
+        NotFound = 404,
+        InternalServerError = 500
+    }
+}
